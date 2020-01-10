e416f27160cb698119458d8c47165d3de1baccfb
Evert Joubert <evert.joubert@gmail.com>
2020-01-10 14:56:47 +0200

    improve AddResource in Rquest

--- network/request.go
+++ network/request.go
@@ -152,51 +152,54 @@ func (reqst *Request) AddResource(resource ...string) {
 		if len(reqst.resources)>0 {
 			lastrsri=len(reqst.resources)-1
 		}
-		for len(resource)>0 {
-			var res=resource[0]
-			resource=resource[1:]
-			if res!="" {
-				if strings.Index(res, "|") > 0 {
-					var rsrs=[]string{}
-					for strings.Index(res, "|") > 0 {
-						var rs=res[:strings.Index(res, "|")]
-						res=res[strings.Index(res, "|")+1:] 
-						if rs!="" {
-							rsrs=append(rsrs,rs)
+		var finalresource=[]string{}
+		for len(finalresource)>0 || len(resource)>0  {
+			if len(resource)>0 {
+				var res=resource[0]
+				resource=resource[1:]
+				if res!="" {
+					if strings.Index(res, "|") > 0 {
+						for strings.Index(res, "|") > 0 {
+							var rs=res[:strings.Index(res, "|")]
+							res=res[strings.Index(res, "|")+1:] 
+							if rs!="" {
+								finalresource=append(finalresource,rs)
+							}
 						}
-					}
-					if res!="" {
-						rsrs=append(rsrs,res)
-					}
-					if len(rsrs)>0 {
-						resource=append(resource,rsrs...)
-						continue
-					}
-				} else {
-					if rsrc := reqst.NewResource(res); rsrc != nil {
-						reqst.resourcesSize = reqst.resourcesSize + rsrc.size
-						if len(reqst.resources) == 0 {
-							reqst.resources = []*Resource{}
+						if res!="" {
+							finalresource=append(finalresource,res)
 						}
-			
-						var prersrs []*Resource
-						var postrsrs []*Resource
-			
-						var currsrs []*Resource = reqst.resources
-			
-						prersrs = currsrs[:lastrsri]
-						postrsrs = currsrs[lastrsri:]
-						var nextrsrs = append(append(prersrs, rsrc), postrsrs...)
-						reqst.resources = nil
-						reqst.resources = nextrsrs[:]
-						prersrs = nil
-						postrsrs = nil
-						currsrs = nil
-						nextrsrs = nil
-						lastrsri++
+					} else {
+						finalresource=append(finalresource,res)
 					}
 				}
 			}
+			for len(finalresource)>0 {
+				var fres=finalresource[0]
+				finalresource=finalresource[1:]
+				if rsrc := reqst.NewResource(fres); rsrc != nil {
+					reqst.resourcesSize = reqst.resourcesSize + rsrc.size
+					if len(reqst.resources) == 0 {
+						reqst.resources = []*Resource{}
+					}
+		
+					var prersrs []*Resource
+					var postrsrs []*Resource
+		
+					var currsrs []*Resource = reqst.resources
+		
+					prersrs = currsrs[:lastrsri]
+					postrsrs = currsrs[lastrsri:]
+					var nextrsrs = append(append(prersrs, rsrc), postrsrs...)
+					reqst.resources = nil
+					reqst.resources = nextrsrs[:]
+					prersrs = nil
+					postrsrs = nil
+					currsrs = nil
+					nextrsrs = nil
+					lastrsri++
+				}
+			}			
 		}
 	}
 }
