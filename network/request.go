20732078ef88a1693350e4445cdb77ed075719ae
Evert Joubert <evert.joubert@gmail.com>
2020-01-14 11:52:15 +0200

    minor update

--- network/request.go
+++ network/request.go
@@ -225,7 +225,7 @@ func (reqst *Request) ExecuteRequest() {
 	}
 	if isAtv {
 		if reqst.rstContent==nil {
-			reqst.rstContent=iorw.NewBufferedRW(rqst.r.Body,maxbufsize)
+			reqst.rstContent=iorw.NewBufferedRW(int64(maxbufsize),rqst.r.Body)
 		}
 	}
 	var mimedetails = mime.FindMimeTypeByExt(reqst.r.URL.Path, ".txt", "text/plain")
