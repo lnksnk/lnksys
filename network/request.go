017071ba5563ea81bd370002b7c2f4ba6e9e913b
Evert Joubert <evert.joubert@gmail.com>
2020-01-27 11:12:41 +0200

    miner update

--- network/request.go
+++ network/request.go
@@ -623,11 +623,12 @@ func (reqst *Request) Write(p []byte) (n int, err error) {
 				for {
 					np,nperr:=wpipeR.Read(npp)
 					if np>0 {
-						if nwp, nwperr := wo.Write(npp[:n]); nwp > 0 && nwperr == nil {
+						if nwp, nwperr := wo.Write(npp[:n]); nwp > 0 {
 							if f, ok := wo.(http.Flusher); ok {
 								f.Flush()
 							}
-						} else if nwperr!=nil {
+						}
+						if nwperr!=nil {
 							nperr=nwperr
 						}
 					}
