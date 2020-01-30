a1847c221b113aa78d3a1939cc84b899c46e6fd6
Evert Joubert <evert.joubert@gmail.com>
2020-01-30 10:50:54 +0200

    miner update

--- lnks.go
+++ lnks.go
@@ -6,13 +6,13 @@ import (
 	lnks "github.com/efjoubert/lnksys/lnks"
 	network "github.com/efjoubert/lnksys/network"
 	os "os"
-	runtime "runtime"
-	runtimedbg "runtime/debug"
+	//runtime "runtime"
+	//runtimedbg "runtime/debug"
 )
 
 func main() {
-	runtimedbg.SetGCPercent(25)
-	runtime.GOMAXPROCS(runtime.NumCPU() * 8)
+	//runtimedbg.SetGCPercent(25)
+	//runtime.GOMAXPROCS(runtime.NumCPU() * 8)
 
 	RunService(os.Args...)
 }
