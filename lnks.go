3876a6739b88687682bd84ce4a8db89121a659be
Evert Joubert <evert.joubert@gmail.com>
2020-02-03 11:19:07 +0200

    cleanup lnks

--- lnks.go
+++ lnks.go
@@ -2,13 +2,8 @@ package main
 
 import (
 	lnks "github.com/efjoubert/lnksys/lnks"
-	//runtime "runtime"
-	//runtimedbg "runtime/debug"
 )
 
 func main() {
-	//runtimedbg.SetGCPercent(25)
-	//runtime.GOMAXPROCS(runtime.NumCPU() * 8)
-
 	lnks.RunService()
 }
